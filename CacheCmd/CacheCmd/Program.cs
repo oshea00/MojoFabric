ae4b3e09304f151614db7daebef563dc0405912e
Mike OShea <oshea00@gmail.com>
2011-12-05 11:58:44 -0800

    Added notification handling

--- CacheCmd/CacheCmd/Program.cs
+++ CacheCmd/CacheCmd/Program.cs
@@ -32,8 +32,13 @@ namespace CacheCmd
                 name = "default";
 
             List<DataCacheServerEndpoint> servers = new List<DataCacheServerEndpoint>(1);
-            servers.Add(new DataCacheServerEndpoint("sead-gbyd1r1-x2",22233));
+            servers.Add(new DataCacheServerEndpoint("sead-gbyd1r1-x2", 22233));
+            servers.Add(new DataCacheServerEndpoint("tac-vdi087", 22233));
             DataCacheFactoryConfiguration configuration = new DataCacheFactoryConfiguration();
+
+            // Set notification properties in the config:
+            configuration.NotificationProperties = new DataCacheNotificationProperties(10000, new TimeSpan(0, 0, 5));
+
             configuration.Servers = servers;
             configuration.LocalCacheProperties = new DataCacheLocalCacheProperties();
             DataCacheClientLogManager.ChangeLogLevel(System.Diagnostics.TraceLevel.Off);
@@ -48,22 +53,41 @@ namespace CacheCmd
         static void Main(string[] args)
         {
             var cache = CacheUtil.GetCache("default");
+            var changeAuthor = args[0] ?? "";
+
+            cache.AddCacheLevelCallback(DataCacheOperations.ReplaceItem |
+                                        DataCacheOperations.AddItem |
+                                        DataCacheOperations.RemoveItem, CacheEvent);
 
             var book = (Book) cache.Get("book_01");
 
             if (book == null)
             {
                 Console.WriteLine("Cache Empty - creating item...");
-                cache.Add("book_01",new Book() {Author = "Mike O'Shea", Title = "How to Use Appfabric", Published = DateTime.Now});
+                cache.Add("book_01",
+                          new Book() {Author = "Mike O'Shea", Title = "How to Use Appfabric", Published = DateTime.Now});
                 book = (Book) cache.Get("book_01");
             }
 
-            Console.WriteLine("Got: "+book);
+            Console.WriteLine("Got: " + book);
+
+            if (!String.IsNullOrEmpty(changeAuthor))
+            {
+                book.Author = changeAuthor;
+                cache.Put("book_01", book);
+            }
 
             var line = Console.ReadLine();
 
             cache.Remove("book_01");
 
         }
+
+        static void CacheEvent(string cacheName, string regionName, string itemName, 
+                               DataCacheItemVersion v, DataCacheOperations ops, DataCacheNotificationDescriptor d)
+        {
+            Console.WriteLine(String.Format("CacheEvent Occurred: {0} {1} {2} {3} {4} {5}",cacheName,regionName,itemName,v,ops,d));
+        }
+
     }
 }
